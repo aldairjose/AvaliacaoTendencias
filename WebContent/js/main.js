ae628a1c1fe2bbe1e371d45ad9905a817643d3ca
TCCSENAI <aldairjvieira@hotmail.com>
2014-07-02 21:04:24 -0300

    Single page Application

---
+++ WebContent/js/main.js
@@ -0,0 +1,48 @@
+document.write('<h1>Formulario<h1>');
+
+tabela = document.createElement('table');
+trh = document.createElement('tr');
+th = document.createElement('th');
+th.innerText = 'Nome';
+
+tabela.appendChild(trh).appendChild(th);
+
+formulario = document.createElement('form');
+
+nome = document.createElement('input');
+nome.setAttribute('placeholder', 'Digite o nome');
+
+
+
+
+tabela.appendChild(trh).appendChild(th);
+
+formulario = document.createElement('form');
+
+nome = document.createElement('input');
+nome.setAttribute('placeholder', 'Digite o nome');
+
+
+idade = document.createElement('input');
+idade.setAttribute('placeholder', 'Digite a idade');
+
+function adicionar(evento){
+	evento.preventDefault();
+	var tr = document.createElement('tr');
+	var td = document.createElement('td');
+	td.innerText = nome.value;
+	
+	nome.value = '';
+
+	tabela.appendChild(tr).appendChild(td);
+}
+
+botaoSalvar = document.createElement('button');
+botaoSalvar.innerText = 'Adicionar';
+botaoSalvar.addEventListener('click', adicionar);
+
+formulario.appendChild(nome);
+formulario.appendChild(botaoSalvar);
+document.body.appendChild(formulario);
+document.body.appendChild(tabela);
+
